24cc3c7aab414566302cf6b7176a35344a3a8471
Damith Premasiri <damithpremasiri@gmail.com>
2022-10-26 16:04:44 +0100

    off-multiprocessing

--- experiments/classification/t5_basics.py
+++ experiments/classification/t5_basics.py
@@ -7,21 +7,24 @@ logging.basicConfig(level=logging.INFO)
 transformers_logger = logging.getLogger("transformers")
 transformers_logger.setLevel(logging.WARNING)
 
-
 train_data = [
-    ["binary classification", "Anakin was Luke's father" , 1],
-    ["binary classification", "Luke was a Sith Lord" , 0],
-    ["generate question", "Star Wars is an American epic space-opera media franchise created by George Lucas, which began with the eponymous 1977 film and quickly became a worldwide pop-culture phenomenon", "Who created the Star Wars franchise?"],
-    ["generate question", "Anakin was Luke's father" , "Who was Luke's father?"],
+    ["binary classification", "Anakin was Luke's father", 1],
+    ["binary classification", "Luke was a Sith Lord", 0],
+    ["generate question",
+     "Star Wars is an American epic space-opera media franchise created by George Lucas, which began with the eponymous 1977 film and quickly became a worldwide pop-culture phenomenon",
+     "Who created the Star Wars franchise?"],
+    ["generate question", "Anakin was Luke's father", "Who was Luke's father?"],
 ]
 train_df = pd.DataFrame(train_data)
 train_df.columns = ["prefix", "input_text", "target_text"]
 
 eval_data = [
-    ["binary classification", "Leia was Luke's sister" , 1],
-    ["binary classification", "Han was a Sith Lord" , 0],
-    ["generate question", "In 2020, the Star Wars franchise's total value was estimated at US$70 billion, and it is currently the fifth-highest-grossing media franchise of all time.", "What is the total value of the Star Wars franchise?"],
-    ["generate question", "Leia was Luke's sister" , "Who was Luke's sister?"],
+    ["binary classification", "Leia was Luke's sister", 1],
+    ["binary classification", "Han was a Sith Lord", 0],
+    ["generate question",
+     "In 2020, the Star Wars franchise's total value was estimated at US$70 billion, and it is currently the fifth-highest-grossing media franchise of all time.",
+     "What is the total value of the Star Wars franchise?"],
+    ["generate question", "Leia was Luke's sister", "Who was Luke's sister?"],
 ]
 eval_df = pd.DataFrame(eval_data)
 eval_df.columns = ["prefix", "input_text", "target_text"]
@@ -33,6 +36,7 @@ model_args.no_save = True
 model_args.evaluate_generated_text = True
 model_args.evaluate_during_training = True
 model_args.evaluate_during_training_verbose = True
+model_args.use_multiprocessing = False
 
 model = T5Model("t5", "t5-base", args=model_args)
 
