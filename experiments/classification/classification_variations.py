6afca5d0d646f7747da78b80c9efe668d660cbc4
Damith Premasiri <damithpremasiri@gmail.com>
2023-02-17 18:25:40 +0000

    rename column name

--- experiments/classification/classification_variations.py
+++ experiments/classification/classification_variations.py
@@ -157,6 +157,7 @@ def run():
         dev_df, df_finetune = train_test_split(dev_df, test_size=0.2, random_state=777)
     elif dataset.__eq__('case-2021'):
         train_df = pd.read_json('data/processed/case-2021/train.json')
+        train_df.rename(columns={'label': 'labels'})
         train_df, test_df = train_test_split(train_df, test_size=0.2, random_state=777)
         train_df, dev_df = train_test_split(train_df, test_size=0.1, random_state=777)
         train_df, df_finetune = train_test_split(dev_df, test_size=0.1, random_state=777)
